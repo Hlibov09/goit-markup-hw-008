67c6b90873247d66d119028fcc4bcae449a43e9f
Hlibov09 <glibov09@gmail.com>
2022-08-21 22:55:15 +0300

    hw

--- js/mobile-minu.js
+++ js/mobile-minu.js
@@ -1,16 +1,47 @@
 (() => {
+  const menuOpenBtn = document.querySelector('[data-menu-open]');
+  const menuCloseBtn = document.querySelector('[data-menu-close]');
+  const mobileMenu = document.querySelector('[data-menu]');
+  const body = document.querySelector('body');
+
+  menuOpenBtn.addEventListener('click', openModal);
+  menuCloseBtn.addEventListener('click', openModal);
+
+  function openModal() {
+    mobileMenu.classList.toggle('is-open');
+    body.classList.toggle('no-scroll');
+  }
+})();
+________________________________________;
+/*
+(() => {
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
-    body: document.querySelector('.body'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('is-open');
   }
 })();
+________________________________________________________
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modall-open]'),
+    closeModalBtn: document.querySelector('[data-modall-close]'),
+    modal: document.querySelector('[data-modall]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+*/
